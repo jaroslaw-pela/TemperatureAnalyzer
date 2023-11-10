407b3d8becdc9d4a90370554698bee88ad916ab5
Jarek <jaroslaw.pela@wp.pl>
2023-11-11 00:19:19 +0100

    Add project files.

---
+++ TemperatureAnalyzer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureAnalyzer
+{
+    public class TemperatureAnalyzer
+    {
+
+        // Znajduje najwyższą temperaturę w tablicy temperatur
+        public static int FindHighestTemperature(int[] temperatures)
+        {
+            // TODO: Uzupełnij implementację, znajdując najwyższą temperaturę
+            // wśród wszystkich temperatur w tablicy.
+            // Zwróć wartość najwyższej temperatury.
+        }
+
+        // Znajduje najniższą temperaturę w tablicy temperatur
+        public static int FindLowestTemperature(int[] temperatures)
+        {
+            // TODO: Uzupełnij implementację, znajdując najniższą temperaturę
+            // wśród wszystkich temperatur w tablicy.
+            // Zwróć wartość najniższej temperatury.
+        }
+
+    }
+}
